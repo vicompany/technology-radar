99f0bcbde5f0f402a59b11b5f6061af4c08d170e
timseverien <timseverien@gmail.com>
2018-05-29 13:29:20 +0200

    Fix version deltas after changing version (fixes #6)

--- docs/assets/scripts/main.js
+++ docs/assets/scripts/main.js
@@ -10,6 +10,14 @@ TechnologyService.setTechnologyRepository(TechnologyRepository);
 const elementTechnologyList = $refs.get('technology-list');
 const elementHistoricDataPicker = $refs.get('container-historic-data-picker');
 
+const getPreviousVersion = (versions, version) => {
+	const versionIndex = versions.indexOf(version);
+
+	return versionIndex > 0
+		? versions[versionIndex - 1]
+		: null;
+};
+
 const loadVersion = async (version, versionPrevious) => {
 	TechnologyService.dispose();
 
@@ -23,9 +31,9 @@ const loadVersion = async (version, versionPrevious) => {
 	const versions = await TechnologyService.getVersions();
 	const versionPicker = new VersionPicker(elementHistoricDataPicker, versions);
 	const versionCurrent = versions[versions.length - 1];
-	const versionPrevious = versions[versions.length - 2];
+	const versionPrevious = getPreviousVersion(versions, versionCurrent);
 
-	versionPicker.on('change', version => loadVersion(version));
+	versionPicker.on('change', version => loadVersion(version, getPreviousVersion(versions, version)));
 	versionPicker.setVersion(versionCurrent);
 	loadVersion(versionCurrent, versionPrevious);
 })();
